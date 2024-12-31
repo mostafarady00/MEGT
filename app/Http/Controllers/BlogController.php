eaa1823cd3d51bf0ba1455c4eb4a69cf779d6758
Mostafa Rady00 <131813659+mostafarady00@users.noreply.github.com>
2024-12-31 16:52:29 +0200

    Update BlogController.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
@@ -11,7 +10,6 @@ class BlogController extends Controller
     public function index()
     {
         $blogs = Blog::all()->map(function ($blog) {
-            $blog->image = url('public/storage/' . $blog->image);
             return $blog;
         });
 
@@ -21,7 +19,6 @@ class BlogController extends Controller
     public function show($id)
     {
         $blog = Blog::findOrFail($id);
-        $blog->image = url('public/storage/' . $blog->image);
 
         return response()->json($blog);
     }
@@ -31,17 +28,13 @@ class BlogController extends Controller
         $request->validate([
             'title_ar' => 'required|string|max:255',
             'title_en' => 'required|string|max:255',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
             'description_ar' => 'required|string',
             'description_en' => 'required|string',
         ]);
 
-        $path = $request->file('image')->store('blogs', 'public');
-
         $blog = Blog::create([
             'title_ar' => $request->title_ar,
             'title_en' => $request->title_en,
-            'image' => $path,
             'description_ar' => $request->description_ar,
             'description_en' => $request->description_en,
         ]);
@@ -56,17 +49,9 @@ class BlogController extends Controller
         $request->validate([
             'title_ar' => 'sometimes|string|max:255',
             'title_en' => 'sometimes|string|max:255',
-            'image' => 'sometimes|image|mimes:jpeg,png,jpg,gif|max:2048',
             'description_ar' => 'sometimes|string',
             'description_en' => 'sometimes|string',
         ]);
-
-        if ($request->hasFile('image')) {
-            Storage::disk('public')->delete($blog->image);
-
-            $path = $request->file('image')->store('blogs', 'public');
-            $blog->image = $path;
-        }
 
         $blog->update($request->only('title_ar', 'title_en', 'description_ar', 'description_en'));
 
@@ -77,11 +62,8 @@ class BlogController extends Controller
     {
         $blog = Blog::findOrFail($id);
 
-        Storage::disk('public')->delete($blog->image);
-
         $blog->delete();
 
         return response()->json(['message' => 'Blog deleted successfully.']);
     }
-
 }
